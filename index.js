21136e496a8a95e7c9301e9b15fed31cd388248a
Nelly Putpana <nelly.putpana@gmail.com>
2023-03-13 19:54:43 -0600

    objects laborat

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+const employee = {name: "Nelly", streetAddress: "Apple street",};
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {return {...employee, [key]: value,};};
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {employee[key] = value;
+     return employee};
+
+function deleteFromEmployeeByKey (employee, key) {const newEmployee = {... employee[key]}; 
+delete newEmployee [key]; return newEmployee;};
+
+function destructivelyDeleteFromEmployeeByKey (employee, key)
+ {delete employee [key]; return employee};
+
+
+
